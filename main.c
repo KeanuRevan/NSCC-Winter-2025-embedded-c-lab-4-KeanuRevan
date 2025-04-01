4a46977f93fe6a45fddcd94936f018ffea82edf9
KeanuRevan <98adthomas@gmail.com>
2025-04-01 09:33:18 -0300

    clion debuging enabled

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include <stdint.h>
 #include <stdio.h>
 
 extern void _print_ch(const char *);
@@ -6,7 +7,15 @@ void print_ch(char ch) {
     _print_ch(&ch);
 }
 
+#define GPIOA_BASE = 0x50000000
+#define GPIOA_MODER  ((volatile uint32_t *)0x50000000)
+// #define GPIOA_MODER  (unsigned long *)0x50000000)
+
+
 int main() {
+
+    *GPIOA_MODER &= ~0x00000800UL;
+
     while(1) {
         /* empty */
         print_ch('X');
