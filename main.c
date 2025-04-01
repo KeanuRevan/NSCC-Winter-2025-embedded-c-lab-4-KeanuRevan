3463cf443e337ef2fdff96890e20c7487c114b4c
KeanuRevan <98adthomas@gmail.com>
2025-04-01 14:13:27 -0300

    Coding Task 2 Complete

--- main.c
+++ main.c
@@ -6,6 +6,7 @@ extern void _print_ch(const char *);
 void print_ch(char ch) {
     _print_ch(&ch);
 }
+
 #define RCC_BASE 0x40021000
 #define RCC_IOPENR (unsigned long *)0x40021034
 
@@ -19,8 +20,7 @@ void print_ch(char ch) {
 
 int main() {
 
-    // *RCC_IOPENR = 0x00000001UL;  // Enable GPIOA clock
-    *RCC_IOPENR = 0x00000001UL;  // Enable GPIOA and GPIOC register clock
+    *RCC_IOPENR |= 0x00000005UL;    // Enable GPIOA and GPIOC register clock
 
     // // Set PA5 as output
     *GPIOA_MODER &= ~0x00000800UL;
@@ -28,21 +28,24 @@ int main() {
     //set PC13 as a digital input
     *GPIOC_MODER &= ~0x0C000000UL;
 
+    bool pressed = false;
+
     while (1) {
 
         if (*GPIOC_IDR & 0x00002000UL) {
             *GPIOA_ODR &= ~0x00000020UL;  // Turn LED off
+            if (pressed == false) {
+                pressed = true;
+                print_ch('N');
+            }
+
         } else {
             *GPIOA_ODR |= 0x00000020UL;   // Turn LED on
+            if (pressed == true) {
+                pressed = false;
+                print_ch('Y');
+            }
         }
-
-        // if (*GPIOA_ODR & 0x00000020UL) {
-        //     *GPIOA_ODR &= ~0x00000020UL;  // Turn LED off
-        // } else {
-        //     *GPIOA_ODR |= 0x00000020UL;   // Turn LED on
-        // }
-
-        // print_ch('Y');
     }
     return 0;
 }
